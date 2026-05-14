c3a96caa787819f4642832b2bf60c7d7f1c1bf24
kamil-matyja <matyja_kamil@o2.pl>
2026-05-14 18:05:41 +0200

    Add swagger

---
+++ app/OpenApi.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *     title="SMS Gateway API",
+ *     version="1.0.0",
+ *     description="Dokumentacja API dla SMS Gateway."
+ * )
+ */
+class OpenApi {}
